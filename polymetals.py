9f047c3a27247474f309ab9c942bcc284d432f2d
Levi C <72059421+Skyclad0bserver@users.noreply.github.com>
2021-05-11 18:05:50 -0700

    Update polymetals.py
    Made code a good bit smaller by using lists for metals and prices instead. Not sure if its more optimal, but it makes it easier for me to add more metals. Also added palladium

--- polymetals.py
+++ polymetals.py
@@ -7,27 +7,10 @@ from yahoo_fin import stock_info as si
 import argparse
 roundBy = 1
 
-
-#Define PM types and get price from yahoo.
-def silver():
-    price = si.get_live_price("SI=F")
-    output = str(round(price, roundBy))
-    return output
-
-def gold():
-    price = si.get_live_price("GC=F")
-    output = str(round(price, roundBy))
-    return output
-
-def platinum():
-    price = si.get_live_price("PL=F")
-    output = str(round(price, roundBy))
-    return output
-
-def copper():
-    price = si.get_live_price("HG=F")
-    output = str(round(price, roundBy))
-    return output
+metals = ['SI=F', 'GC=F', 'PL=F', 'PA=F', 'HG=F']
+prices = []
+for i in range(len(metals)):
+    prices.insert(i, str(round(si.get_live_price(metals[i]), roundBy)))
 
 def displayArgs():
     #Args to display
@@ -35,25 +18,29 @@ def displayArgs():
     parser.add_argument("-s", '--silver', help='display the price of silver (in troy ounces).', action="store_true")
     parser.add_argument("-g", '--gold', help='display the price of gold (in troy ounces).', action="store_true")
     parser.add_argument("-p", '--platinum', help='display the price of platinum (in troy ounces).', action="store_true")
+    parser.add_argument("-a", '--palladium', help='display the price of palladium (in troy ounces).', action="store_true")
     parser.add_argument("-c", '--copper', help='display the price of copper (in pounds).', action="store_true")
     args = parser.parse_args()
     metal = ""
     
     #Parse the args
     if args.silver:
-        metal += "Ag: $" + silver() + "/oz | "
+        metal += "Ag: $" + prices[0] + "/oz | "
     if args.gold:
-        metal += "Au: $" + gold() + "/oz | "
+        metal += "Au: $" + prices[1] + "/oz | "
     if args.platinum:
-        metal += "Pt: $" + platinum() + "/oz | "
+        metal += "Pt: $" + prices[2] + "/oz | "
+    if args.palladium:
+        metal += "Pd: $" + prices[3] + "/oz | "
     if args.copper:
-        metal += "Cu: $" + copper() + "/lb | "
+        metal += "Cu: $" + prices[4] + "/lb | "
         
     #Display the args (if they exist)
     if metal == "":
-        print("No metal chosen to display!")
+        print("No metal chosen to display! Use --help to know more!")
     else:
         print(metal)
 
 if __name__ == '__main__':
     displayArgs()
+
